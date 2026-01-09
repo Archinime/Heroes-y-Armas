276f8c9c9d79f5843d042d86a7620b2b0365c6c1
Archinime <archinime12@gmail.com>
2026-01-09 11:07:46 -0500

    Add files via upload

---
+++ musica-data.js
@@ -0,0 +1,222 @@
+    const audioPlaylists = {
+        1: [ // Dan Da Dan
+            "dandadan.mp3"
+        ],
+        2: [ // Jujutsu
+            "jujutsukaisen.mp3"
+        ],
+        3: [ // Konosuba
+            "konosuba.mp3"
+        ],
+        4: [ // Re:Zero
+            "「Call of the Witch」ReZero OST 15 ／『Main Soundtrack』.mp3",
+            "ReZERO - Starting Life in Another World - Ending.mp3",
+            "ReZERO -Starting Life in Another World- Season 2 - Ending Memento.mp3"
+        ],
+        5: [ // Mushoku Tensei
+            "mushokutensei.mp3"
+        ],
+        6: [ // Tensei Shitara Slime Datta Ken
+            "tenseislime.mp3"
+        ],
+        7: [ // Saga of Tanya the Evil (Youjo Senki)
+            "tayna.mp3"
+        ],
+        8: [ // Akame Ga Kill
+            "akame.mp3"
+        ],
+        9: [ // To Be Hero X
+            "tobeherox.mp3"
+        ],
+        10: [ // Demon Slayer
+            "demonslayer1.mp3",
+            "demonslayer2.mp3",
+            "demonslayer3.mp3"
+        ],
+        11: [ // Maou 2099
+            "maou2099.mp3"
+        ],
+        12: [ // Tsue to Tsurugi no Wistoria
+            "Wistoria Wand and Sword - Opening  Fire and Fear.mp3"
+        ],
+        13: [ // Izure Saikyou no Renkinjutsushi
+            "izure.mp3"
+        ],
+        14: [ // Alya Sometimes Hides Her Feelings in Russian
+            "alyarusa.mp3"
+        ],
+        15: [ // Reborn as a Vending Machine
+            "Rebornasa vending.mp3"
+        ],
+        16: [ // Kimi to Boku no Saigo no Senjou
+            "Kimi to Boku no Saigo.mp3"
+        ],
+        17: [ // The Gorila God's Go- To Girl
+            "The Gorilla Gods Go- To Girl  illuminate - Nakamura Shuugo「KanRomPer」.mp3"
+        ],
+        18: [ // Chainsaw Man
+            "Chainsaw Man – The Movie Reze Arc - Theme Song FULL IRIS OUT by Kenshi Yonezu (Lyrics).mp3"
+        ],
+        19: [ // The Rising of the Shield Hero
+            "MADKID  RISE (The Rising of the Shield Hero Opening Theme).mp3"
+        ],
+        20: [ // Overflow
+            "『Hinooka Rei - Uzuho』 Overflow Ending Full  ϟ Sub. Español - Romaji ♡.mp3"
+        ],
+        21: [ // Shiunji-ke no Kodomotachi
+            "Shiunji-ke no Kodomo-tachi Opening Full  Honey Lemon - Sub. Español ♡.mp3"
+        ],
+        22: [ // Goblin Slayer
+            "Goblin Slayer OP Full  Rightfully - Sub Español.mp3"
+        ],
+        23: [ // Kanchigai no Atelier Meister
+            "Unaware Atelier Meister - Opening full FACTSORY by MeseMoa [Lyrics].mp3"
+        ],
+        24: [ // Definitivamente. ¡No me gusta mi hermano para nada!
+            "Onii-chan no Koto Nanka Zenzen Suki Janain Dakara Ne!!  ED FULL.mp3"
+        ],
+        25: [ // Kaiju No. 8
+            "アニメ『怪獣８号』ノンクレジットED｜OneRepublic「Nobody」｜毎週土曜23時～放送・配信.mp3"
+        ],
+        26: [ // Zootopia
+            "Zootopia - Try Everything By Shakira (Music Video).mp3"
+        ],
+        27: [ // Clevatess -Majuu no Ou to Akago
+            "clevates.mp3"
+        ],
+        28: [ // Dr. Stone
+            "Dr. Stone  Good Morning World!  Burnout Syndromes AMV ( sub español ).mp3",
+            "ending4 doctor stone.mp3"
+        ],
+        29: [ // One Piece (Live Action)
+            "We Are. OP. 1  One Piece.  Traduccion Al Español.mp3"
+        ],
+        30: [ // Shoujo Ramune
+            "Shoujo Ramune (Anime) Opening (小女ラムネ OVA OP - 挿入歌 ).mp3"
+        ],
+        31: [ // To Your Eternity
+            "To Your Eternity Opening  Hikaru Utada -  Pink Blood  Sub Español - Lyrics Romaji.mp3"
+        ],
+        32: [ // Tsugunai Subbed
+            "Tsugunai Subbed.mp3"
+        ],
+        33: [ // Blue Lock
+            "Blue Lock OP Full  Chaos ga Kiwamaru  Sub Español - Romaji『AMV』☆.mp3"
+        ],
+        34: [ // Magic Maker: Isekai Mahou no Tsukurikata
+            "Magic Maker Isekai Mahou no Tsukurikata ending full  Yoake no Uta (夜明けの歌) by Humbreaders.mp3"
+        ],
+        35: [ // Tsuyokute New Saga
+            "New Saga Ending - Her with KanRomEn.mp3"
+        ],
+        36: [ // Gachiakuta
+            "Gachiakuta - Ending FULL TOMOSHIBI by DUSTCELL (Lyrics).mp3"
+        ],
+        37: [ // Shinsei Kourin Dacryon Luna
+            "Ending Shinsei Kourin Dacryon.mp3"
+        ],
+        38: [ // Kami-tachi ni Hirowareta Otoko
+            "Kami Tachi ni Hirowareta Otoko OP  Azusa Tadokoro - Yasashii Sekai Sub Español, Romaji & English.mp3"
+        ],
+        39: [ // Ore dake Haireru Kakushi Dungeon
+            "Pyramid Daigyakuten - by Spira Spica - Ore dake Haireru Kakushi Dungeon Opening.mp3"
+        ],
+        40: [ // One Punch-Man
+            "One punch man - The Hero  OP 1  sub. español ; lyrics ; letra.mp3"
+        ],
+        41: [ // Mashle
+            "Bling-Bang-Bang-Born.mp3"
+        ],
+        42: [ // Los Diarios de la Boticaria
+            "Kusuriya no Hitorigoto ed 1.mp3",
+            "Kusuriya no Hitorigoto Season 2 Opening Full  Hyakka Ryouran by Lilas Ikuta - Sub. Español ♡.mp3",
+            "Kusuriya no Hitorigoto 3.mp3",
+        ],
+        43: [ // Maplestar
+            "phub.mp3"
+        ],
+        44: [ // Uzumaki
+            "Uzumaki  Opening theme Song   Intro Anime.mp3"
+        ],
+        45: [ // Tondemo Skill de Isekai Hourou Meshi
+            "Tondemo Skill de Isekai Hourou Meshi Opening Full  Luxury Spoon -  Van de Shop  Sub Español Latino.mp3"
+        ],
+        46: [ // My Dress-Up Darling
+            "My Dress-Up Darling Season 2 Opening Theme).mp3"
+        ],
+        47: [ // Hazbin Hotel
+            "Poison (Español Latino) - Hazbin Hotel Fan Lyric Vídeo.mp3"
+        ],
+        48: [ // The Eminence in Shadow
+            "The Eminence in Shadow  Moonlight Sonata (1st Movement) - Beethoven.mp3",
+            "The Eminence in Shadow Season 2 OP Full  grayscale dominator  Sub Español - Romaji『AMV』☆.mp3"
+        ],
+        49: [ // Spy × Family
+            "Spy x Family Ed. 2 Full  Shikisai by yama  Sub Español.mp3"
+        ],
+        50: [ // Black Clover
+            "Black Clover.mp3"
+        ],
+        51: [ // My Hero Academia
+            "Boku no Hero Academia Season 1 Ending 1 full『 Brian the Sun - Heroes』【Sub EspañolJaponés】AMV.mp3"
+        ],
+        52: [ // Kakkou no Linazuke
+            "Kakou no iinazuke OP FULL  Kiyoe Yoshioka Dekoboko  Sub Español, Romaji & English-AMV.mp3"
+        ],
+        53: [ // Futari Solo Camp
+            "futari solo camp.mp3"
+        ],
+        54: [ // Tōjima Tanzaburō wa Kamen Rider ni Naritai
+            "Tojima Tanzaburo Wants To Be A Kamen Rider OP FULL - 『Wanna Be』 by TeddyLoid feat. Shigeru Matsuzaki.mp3"
+        ],
+        55: [ // Alma-chan wa Kazoku ni Naritai
+            "Alma-chan wa Kazoku ni Naritai Ending Full『Arifuretetai』by KAF.mp3"
+        ],
+        56: [ // Spice and Wolf
+            "Spice and Wolf (2024) ED  Ending 2 - Creditless  4K  24FPS (1).mp3"
+        ],
+        57: [ // Oyasumi Sex
+            "【Oyasumi Sekusu Episode 4】Ending (1).mp3"
+        ],
+        58: [ // Rick and Morty
+            "rym.mp3"
+        ],
+        59: [ // Akujiki Reijou to Kyouketsu Koushaku
+            "akujiki.mp3"
+        ],
+        60: [ // Saigo ni Hitotsu dake Onegai shitemo Yoroshii deshou ka
+            "saigonihitotsu.mp3"
+        ],
+        61: [ // Ataque a los titanes
+            "syn.mp3",
+            "syn2.mp3"
+        ],
+        62: [ // Record of Ragnarok
+            "record.mp3"
+        ],
+        63: [ // Genshin Impact Hentai
+            "phub.mp3"
+        ],
+        64: [ // Gnosia
+            "gnosia.mp3"
+        ],
+        65: [ // Classroom of the Elite
+            "classroomelite.mp3"
+        ],
+        66: [ // Frieren: Más allá del final del viaje
+            "frieren.mp3"
+        ],
+        67: [ // Solo leveling
+            "sololeveling.mp3"
+        ],
+        68: [ // It - Bienvenidos a Derry
+            "it.mp3"
+        ],
+        69: [ // Cars
+            "cars.mp3"
+        ],
+        70: [ // Kimi to Koete Koi ni Naru
+            "kimitokoete.mp3"
+        ],
+        71: ["overlord.mp3"] 
+    };
